a430e7822281feece389bf3ba3cabf11850ef1b7
Aleksandar Markovic <aleksandar.markovic@omegapoint.se>
2022-11-09 18:46:43 +0100

    fix(#14): Define data structure and topic for table entries stream

--- kafka-snake/src/main/java/op/kompetensdag/snake/Topics.java
+++ kafka-snake/src/main/java/op/kompetensdag/snake/Topics.java
@@ -2,13 +2,15 @@ package op.kompetensdag.snake;
 
 public class Topics {
 
-    static final String GAME_INPUT_TOPIC = "game_input";
-    static final String GAME_MOVEMENT_COMMANDS_TOPIC = "movement_commands";
-    static final String GAME_ADMINISTRATION_COMMANDS_TOPIC = "game_admin_commands";
-    static final String GAME_STATUS_TOPIC = "game_status";
-    static final String HEAD_DIRECTION_TOPIC = "head_direction";
+    public static final String GAME_INPUT_TOPIC = "game_input";
+    public static final String GAME_MOVEMENT_COMMANDS_TOPIC = "movement_commands";
+    public static final String GAME_ADMINISTRATION_COMMANDS_TOPIC = "game_admin_commands";
+    public static final String GAME_STATUS_TOPIC = "game_status";
+    public static final String HEAD_DIRECTION_TOPIC = "head_direction";
 
-    static final String GAME_OUTPUT = "game_output";
+    public static final String GAME_OUTPUT = "game_output";
 
-    static final String CLIENT_RESPONSES_TOPIC = "client_responses";
+    public static final String CLIENT_RESPONSES_TOPIC = "client_responses";
+
+    public static final String GAME_TABLE_ENTRIES = "game-table-entries";
 }
